bd0602cf2087058a6b90a407d5b7741921303638
Anthonyph <anthony.phim@gmail.com>
2019-10-03 15:57:49 +0200

    last commit before readme

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,54 +1,56 @@
-
 #condition de victoire win=10
 #condition initial n = 0;
 #monte d'une marche si le dé fait 5 ou 6; n = n+1
 #descend d'une marche si le dé fait 1; n = n-1
 #ne fait rien si le dé fait 2, 3 ou 4; n = n
-
 def lancer_de_dé
 dice = rand(1..6).to_s
-puts dice 
+#puts dice 
 return dice    
 end
-
-def point(dice)
-
+def valeur_n
+    dice=lancer_de_dé
     case dice
         when "1"
-            #puts " tu as fait un 1"
+            puts "Tu as fait un 1, tu redescends -1"
             n = -1 
         when "2","3","4" 
-            #puts " tu as fait un #{dice}"
+            puts "Tu as fait un #{dice}, ça sert à rien"
             n=0
         when "5","6"
-            #puts " tu as fait un #{dice}"
+            puts "Tu as fait un #{dice}, bravo tu montes +1"
             n=1
     end
     return n
 end
-
-def jeu(n, dice)
+def jeu
     total = 0
     win = 10
-    n
+    tour_number = []
     while total < win
+        n = valeur_n
         total = n + total
-        puts "tu as fait un #{dice} tu es sur la marche #{total}"
+        puts "Tu es sur la marche #{total} !"
+        tour_number << "#{total}" 
     end
-
+    puts " Bravo, tu as gagné.e.s !!!! TOutes mes félicitations !!!!"
+    z = tour_number.length 
+    return z
 end
 
-def statistique
-
+def average_finish_time
+    results_array = []
+    5.times do 
+        z=jeu
+        jeu
+        results_array << z
+    end 
+    p results_array 
+    somme = results_array.sum
+    taille = results_array.length
+puts "En moyenne, il a fallut #{somme/taille} lancé de dé !"
 end
 
-def perform
-    dice = lancer_de_dé
-    n = point(dice)
-
-    jeu(n, dice)
-end
-
-perform
+average_finish_time
 
 
